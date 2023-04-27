b758a5810eb797c5a15c5d75443aa3c55d5c9bab
nguyenthanhnam2901 <127769365+nguyenthanhnam2901@users.noreply.github.com>
2023-04-27 12:25:30 +0700

    StudentRecord.c

--- StudentRecord.c
+++ StudentRecord.c
@@ -7,6 +7,9 @@
 
 int n;
 student students[1003];
+int mx(int a, int b) {
+    return (a > b ? a : b);
+}
 
 void input()
 {
@@ -89,40 +92,54 @@ void input()
         }
     }
 }
-void printToFile()
-{
-    FILE *fp = fopen("studentList.txt", "w");
-    fprintf(fp, "ID\tFullname\tBirthdate\tAlgebra\tCalculus\tBP\tGPA\n");
-    for (int i = 1; i <= n; ++i)
+
+int  numLen(int x) {
+    if(x == 0) return 1;
+    int len = 0;
+    while (x > 0)
     {
-        fprintf(fp, "%s\t%s %s\t%d/%d/%d\t%.2f\t%.2f\t%.2f\t%.2f\n",
-                students[i].id,
-                students[i].fullName.lastName, students[i].fullName.firstName,
-                students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year,
-                students[i].grade[0], students[i].grade[1], students[i].grade[2],
-                gpa(students[i]));
+        x /= 10;
+        len++;
     }
-    fclose(fp);
+    return len;
 }
+int tableLen[7] = {2,9,9,7,8,17,4};
 
 int main()
 {
     input();
-
-    /*
-    printf("ID\tFullname\tBirthdate\tAlgebra\tCalculus\tBP\tGPA\n");
     for(int i = 1; i <= n; ++i) {
-        printf("%s\t%s %s\t%d/%d/%d\t%.2f\t%.2f\t%.2f\t%.2f\n",
-            students[i].id,
-            students[i].fullName.lastName, students[i].fullName.firstName,
-            students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year,
-            students[i].grade[0], students[i].grade[1], students[i].grade[2],
-            gpa(students[i])
-        );
+        tableLen[0] = mx(tableLen[0],strlen(students[i].id));
+        tableLen[1] = mx(tableLen[1],strlen(students[i].fullName.lastName) + strlen(students[i].fullName.firstName) + 1);
+        tableLen[2] = mx(tableLen[2], numLen(students[i].birthDay.day) + numLen(students[i].birthDay.month) + numLen(students[i].birthDay.year) + 2);
     }
-    */
-
-    printToFile();
-
-    return 0;
+    printf("ID");
+    for(int i = 1; i <= tableLen[0] - 2; ++i) printf(" ");
+    printf("\tFull Name"); 
+    for(int i = 1; i <= tableLen[1] - 9; ++i) printf(" ");
+    printf("\tBirthdate");
+    for(int i = 1; i <= tableLen[2] - 9; ++i) printf(" ");
+    printf("\tAlgebra       ");
+    printf("Calculus       ");
+    printf("Basic Programming       ");
+    printf("GPA\n");
+    for(int i = 1; i <= n; ++i) {
+        printf("%s", students[i].id);
+        for(int j = 1; j <= tableLen[0] - strlen(students[i].id); ++j) printf(" ");
+        printf("\t%s %s", students[i].fullName.lastName, students[i].fullName.firstName);
+        for(int j = 1; j <= tableLen[1] - strlen(students[i].fullName.lastName) - strlen(students[i].fullName.firstName); ++j) printf(" ");
+        printf("\t%d/%d/%d", students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year);
+        for(int j = 1; j <= tableLen[2] - (numLen(students[i].birthDay.day) + numLen(students[i].birthDay.month) + numLen(students[i].birthDay.year) + 2); ++j) printf(" ");
+        for(int j = 0; j <= 2; ++j) {
+            if(students[i].grade[j] < 10.0) {
+                printf("       %.2f", students[i].grade[j]);
+                for(int k = 1; k <= tableLen[3 + j] - 4; ++k) printf(" ");
+            }
+            else {
+                printf("       %.2f", students[i].grade[j]);
+                for(int k = 1; k <= tableLen[3 + j] - 5; ++k) printf(" ");
+            }
+        }
+        printf("       %.2f\n",gpa(students[i]));
+    }
 }
